77b72af6e38747dd2d35ea21913ea3a8eaf7cf88
wartman <faxmaster@gmail.com>
2025-03-02 14:44:44 -0600

    A little more cleanup

--- playground/Examples.hx
+++ playground/Examples.hx
@@ -3,64 +3,75 @@ using Kit;
 using kit.Cli;
 
 function main() {
-  var cli = Cli.fromSys();
-  cli.execute(new App());
+	var cli = Cli.fromSys();
+	cli.execute(new App());
 }
 
 class App implements Command {
-  @:flag var off:Bool = false;
-  @:flag('f') var foo:String = null;
-  @:flag var help:Bool = false;
+	/**
+		This does nothing.
+	**/
+	@:flag var off:Bool = false;
 
-  @:command final sub = new SubCommand('pre');
+	/**
+		This does nothing.
+	**/
+	@:flag('f') var foo:String = null;
 
-  public function new() {}
+	/**
+		Get help for a given command
+	**/
+	@:flag var help:Bool = false;
 
-  /**
-    This command will print whatever you give it.
+	@:command final sub = new SubCommand('pre');
 
-    That's all.
-  **/
-  @:command
-  public function other(bar:String = ''):Task<Int> {
-    if (help) {
-      output.write(getDocs());
-      return 0;
-    }
+	public function new() {}
 
-    output
-      .writeLn('We can do fun stuff with styles: '.underscore())
-      .write(' This has a background '.backgroundColor(White).color(Blue).bold())
-      .writeLn(bar.color(Red).bold());
+	/**
+		This command will print whatever you give it.
 
-    return 0;
-  }
+		That's all.
+	**/
+	@:command
+	public function other(bar:String = ''):Task<Int> {
+		if (help) {
+			output.write('Enter a string!'.backgroundColor(White).color(Red));
+			return 0;
+		}
 
-  /**
-    This is a simple app to explain how things work.
-  **/
-  @:defaultCommand
-  public function docs():Task<Int> {
-    output.write(getDocs());
-    return 0;
-  }
+		output
+			.writeLn('We can do fun stuff with styles: '.underscore())
+			.write(' This has a background '.backgroundColor(White).color(Blue).bold())
+			.writeLn(bar.color(Red).bold());
+
+		return 0;
+	}
+
+	/**
+		This is a simple app to explain how things work.
+	**/
+	@:defaultCommand
+	public function docs():Task<Int> {
+		output.write(getDocs());
+		return 0;
+	}
 }
 
 class SubCommand implements Command {
-  @:flag('f') var suffix:String = 'Ok!';
+	@:flag('f') var suffix:String = 'Ok!';
 
-  final prefix:String;
+	final prefix:String;
 
-  public function new(prefix) {
-    this.prefix = prefix;
-  }
+	public function new(prefix) {
+		this.prefix = prefix;
+	}
 
-  /**
-    This also just prints foo.
-  **/
-  @:defaultCommand
-  function doesAThing(foo:String, bin:String = 'ok'):Task<Int> {
-    output.writeLn(prefix + ' ' + foo + bin + ' ' + suffix);
-    return 0;
-  }
+	/**
+		This also just prints foo.
+	**/
+	@:defaultCommand
+	function doesAThing(foo:String, bin:String = 'ok'):Task<Int> {
+		output.writeLn(prefix + ' ' + foo + bin + ' ' + suffix);
+		return 0;
+	}
 }
--- src/kit/cli/DocFormatter.hx
+++
@@ -1,5 +0,0 @@
-package kit.cli;
-
-interface DocFormatter {
-	public function format(spec:DocSpec):String;
-}
